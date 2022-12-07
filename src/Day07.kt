a85bfc301788302fe7276ac3679347101e94b712
nibe <nibe.cons@bene.it>
2022-12-07 18:44:56 +0100

    Day 07 - part2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,11 +1,15 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return getFilesystem(input).directoryList.filter { dir -> dir.getSize() <= 100000 }.sumOf { dir -> dir.getSize() }
+        return getFilesystem(input).directoryList.filter { it.getSize() <= 100000 }.sumOf { it.getSize() }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val memToFree = getFilesystem(input).fileList.sumOf { it.getSize() } - 40000000
+        return getFilesystem(input).directoryList
+            .filter { it.getSize() >= (memToFree) }
+            .minByOrNull { it.getSize() }
+            ?.getSize() ?: -1
     }
 
     val input = readInput("Day07")
@@ -22,25 +26,23 @@ fun getFilesystem(input: List<String>): MyFileSystem {
         if (line.startsWith("$")) {// it's a command
             if (line.contains(" cd ")) {
                 val nextDirName = line.split(" cd ")[1]
-                if (nextDirName.equals("..")) {
+                currentDir = if (nextDirName == "..") {
                     // go up one directory
-                    currentDir = currentDir.parent?:currentDir
+                    currentDir.parent?:currentDir
                 } else {
                     // go down one directory
-                    currentDir = currentDir.subdirectories
-                        .filter { dir -> dir.name.equals(nextDirName) }
-                        .firstOrNull()?: currentDir
+                    currentDir.subdirectories.firstOrNull { dir -> dir.name == nextDirName } ?: currentDir
                 }
             }
         } else { // it's file or directory
             val (objSize, objName) = line.split(" ")
-            if (objSize.equals("dir")) {
+            if (objSize == "dir") {
                 currentDir.subdirectories
-                    .filter { dir -> dir.name.equals(objName) }
+                    .filter { dir -> dir.name == objName }
                     .ifEmpty { currentDir.addSubdirectory(objName) }
             } else {
                 currentDir.files
-                    .filter { file -> file.name.equals(objName) }
+                    .filter { file -> file.name == objName }
                     .ifEmpty { currentDir.addFile(objName, objSize.toInt()) }
             }
         }
@@ -50,14 +52,14 @@ fun getFilesystem(input: List<String>): MyFileSystem {
     return fs
 }
 
-class MyFileSystem(val fsName: String) {
+class MyFileSystem(fsName: String) {
     val root = Directory(this, null, fsName, "/")
     val directoryList = mutableListOf<Directory>()
     val fileList = mutableListOf<File>()
 }
 
-class Directory(val fs: MyFileSystem, val parent: Directory?, val name: String, path: String) {
-    val fullPath = if (name .equals("/") ) "/" else path + name + "/"
+class Directory(private val fs: MyFileSystem, val parent: Directory?, val name: String, path: String) {
+    private val fullPath = if (name == "/") "/" else "$path$name/"
     var subdirectories = mutableListOf<Directory>()
     var files = mutableListOf<File>()
     
@@ -68,27 +70,21 @@ class Directory(val fs: MyFileSystem, val parent: Directory?, val name: String, 
     }
     
     fun addFile(fileName: String, fileSize: Int) {
-        val newFile = File(fileName, fileSize, this.fullPath)
+        val newFile = File(fileName, fileSize)
         files.add(newFile)
         fs.fileList.add(newFile)
     }
     
-    fun isDirectory(): Boolean { return true}
     fun getSize(): Int {
         var sum = 0
         for (i in 0 until files.size) sum += files[i].getSize()
         for (i in 0 until subdirectories.size) sum += subdirectories[i].getSize()
         return sum
     }
-    
-    fun containsDirectory(dirName: String): Boolean {
-        return subdirectories.filter { it.name.equals(dirName)}.size > 0
-    }
+
 }
 
-open class File(open val name: String, val fileSize: Int, val path: String ) {
-    val fullPath = path + "/"
-    
+open class File(open val name: String, private val fileSize: Int) {
     open fun getSize(): Int {
         return fileSize
     }
